a627251ec3c76d1111b2f0d34deff568ffd521d8
avi074 <gautamavinash.ag@gmail.com>
2024-08-13 18:36:43 +0530

    fundamental functions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import Header from "./components/Header";
+import InputModal from "./components/InputModal";
+import ToDoItem from "./components/ToDoItem";
+import ToDoList from "./components/ToDoList";
+import data from "./utils/mockdata";
 
 function App() {
-
   return (
     <>
-     
+      <Header />
+      <ToDoList>
+        <InputModal />
+        {data.map(({ id, name, desc, priority, status }) => {
+          return (
+            <ToDoItem
+              key={id.toString()}
+              name={name}
+              desc={desc}
+              priority={priority}
+              status={status}
+            />
+          );
+        })}
+      </ToDoList>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
