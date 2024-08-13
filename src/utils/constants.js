85ee064ea9ccbc812f240072c9b6d58c40cf489d
avi074 <gautamavinash.ag@gmail.com>
2024-08-13 18:33:38 +0530

    utilities added

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const PRIORITY = Object.freeze(['Low', 'Medium', 'High', 'Critical', 'Urgent'])
+const PCOLOR = Object.freeze(['blue-gray','green', 'yellow', 'orange', 'red'])
+const STATUS = Object.freeze(['Completed', 'Pending', 'Active'])
+const SCOLOR = Object.freeze(['gray', 'amber', 'green'])
+
+export { PRIORITY, PCOLOR, STATUS, SCOLOR }
