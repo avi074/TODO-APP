40f2092545e64290f8ed512291df3398056e067a
avi074 <gautamavinash.ag@gmail.com>
2024-08-15 14:42:16 +0530

    ToDOList & ToDoItem components updated

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -7,39 +7,72 @@ import {
 } from "@material-tailwind/react";
 import { PRIORITY, STATUS, PCOLOR, SCOLOR } from "../utils/constants.js";
 
-function ToDoItem({ name, desc, priority, status }) {
+function ToDoItem({
+  task: { id, name, desc, priority, status },
+  events: { handleEdit, handleDelete, handleStatus },
+}) {
   return (
-    <li className="mx-auto my-4 w-full lg:m-0 lg:px-4">
-      <Card variant="gradient" color="gray" className="">
+    <li className="mx-auto my-4 w-full lg:m-0 lg:px-4" id={`todo-${id}`}>
+      <Card variant="gradient" color="gray">
         <CardBody>
-          <Typography variant="h5" color="white" className="">
+          <Typography variant="h5" color="white">
             {name}
           </Typography>
-          <Typography variant="paragraph" color="amber">
-            {desc}
-          </Typography>
+          <div className="text-amber-700 text-sm">
+            <pre>
+              {desc}
+            </pre>
+          </div>
         </CardBody>
-        <CardFooter>
+        
+        <CardFooter className="pt-0">
           <div className="flex justify-around flex-wrap mb-4">
             <Typography variant="h6" color={PCOLOR[priority]}>
               Priority : {PRIORITY[priority]}
             </Typography>
             <Typography variant="h6" color={SCOLOR[status]}>
-              Status : <Button color={SCOLOR[status]} className="p-2">{STATUS[status]}</Button>
+              Status :{" "}
+              <Button
+                color={SCOLOR[status]}
+                className="p-2"
+                disabled={status == 0}
+                onClick={() => {
+                  handleStatus(id, status == 1 ? 2 : 1);
+                }}>
+                {STATUS[status]}
+              </Button>
             </Typography>
           </div>
+
+          {/* Button Group */}
           <div className="flex justify-around flex-wrap gap-2">
-            <Button variant="outlined" color="blue">
+            <Button
+              variant="outlined"
+              color="blue"
+              onClick={() => {
+                handleEdit(id);
+              }}
+              disabled={status == 0}>
               Edit
             </Button>
-            <Button variant="gradient" color="red">
+
+            <Button
+              variant="gradient"
+              color="red"
+              onClick={() => {
+                handleDelete(id);
+              }}>
               Delete
             </Button>
+
             <Button
               variant="text"
               color="green"
-              className="p-2 shadow-sm shadow-white">
-              Mark as Done
+              className="p-2 shadow-sm shadow-white"
+              onClick={() => {
+                handleStatus(id, status > 0 ? 0 : 1);
+              }}>
+              Mark as {status > 0 ? "Done" : "Undone"}
             </Button>
           </div>
         </CardFooter>
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,11 +1,10 @@
-
-
 function ToDoList(props) {
   return (
-    <div className="flex-card flex-col mx-auto w-[95%] md:w-3/4">
-      {props.children[0]}
+    <div className="flex justify-center items-center flex-col mx-auto w-[95%] md:w-3/4">
+      {props.children[0] /* input modal*/} 
+
       <ol id="todo-list" className="w-full grid grid-cols-1 gap-2 items-center lg:grid-cols-2" >
-        {props.children[1]}
+        {props.children[1] /* todo items as ordered list items */}
       </ol>
     </div>
   );
