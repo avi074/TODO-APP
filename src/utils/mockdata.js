a00ce1eaa59aac507c36185d4fbd2e328cc40fe7
avi074 <gautamavinash.ag@gmail.com>
2024-08-15 14:42:45 +0530

    utils updated

--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -1,30 +1,30 @@
 const data = [
     {
-        id:Symbol(1), // Symbol(Date.now())  --> provide unique ids for every second
+        id:100, // Symbol(Date.now())  --> provide unique ids for every second
         name:'Youtube watching',
         desc:'',
-        priority:3, // 0 -> low, 1 -> average, 2 -> high, 3 -> important, 4 -> urgent 
+        priority:'3', // 0 -> low, 1 -> average, 2 -> high, 3 -> important, 4 -> urgent 
         status: 0 // 0 -> complete, 1 -> pending, 2 -> active 
     },
     {
-        id:Symbol(2), // Symbol(Date.now())  --> provide unique ids for every second
+        id:200, // Symbol(Date.now())  --> provide unique ids for every second
         name:'WebSeries watching',
         desc:'Game of Throns',
-        priority:2,  
+        priority:'2',  
         status: 1 
     },
     {
-        id:Symbol(3), // Symbol(Date.now())  --> provide unique ids for every second
+        id:300, // Symbol(Date.now())  --> provide unique ids for every second
         name:'Game Playing',
         desc:'',
-        priority:1, 
+        priority:'1', 
         status: 2  
     },
     {
-        id:Symbol(4), // Symbol(Date.now())  --> provide unique ids for every second
+        id:400, // Symbol(Date.now())  --> provide unique ids for every second
         name:'Groceries',
         desc:'1.Tomato\n2.Onion\n3.Potato',
-        priority:4, 
+        priority:'4', 
         status: 0 
     },
 ]
